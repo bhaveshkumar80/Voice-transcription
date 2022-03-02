aa93b83860337d67ac51c6ff2c08da24b1bf4808
Bhavesh Kumar <bhaveshkumar2806@gmail.com>
2022-03-02 12:18:09 +0530

    updated model

--- voice_translator.py
+++ voice_translator.py
@@ -1,26 +1,39 @@
+#http://ec2-35-86-155-156.us-west-2.compute.amazonaws.com:5000/docs
+
 import torch
 import librosa
-import soundfile as sf
-import numpy as np
-from scipy.io import wavfile
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+#model_path = "models/"
+model_path = "facebook/wav2vec2-large-960h"
+
+processor = Wav2Vec2Processor.from_pretrained(model_path)
+model = Wav2Vec2ForCTC.from_pretrained(model_path)
 
 tokenizer.save_pretrained("models/")
 model.save_pretrained("models/")
 
 @app.post("/")
 async def Voice_Translation(audio_file: UploadFile = File(...)):
+    try:
+        input_audio, _ = librosa.load(audio_file.file, sr=16000)
 
-    input_audio, _ = librosa.load(audio_file.file, sr=16000)
+        # tokenize
+        input_values = processor(input_audio, return_tensors="pt").input_values  # Batch size 1
 
-    input_values = tokenizer(input_audio, return_tensors="pt").input_values
-    logits = model(input_values).logits
-    predicted_ids = torch.argmax(logits, dim=-1)
-    transcription = tokenizer.batch_decode(predicted_ids)[0]
-    return {'text': transcription}
+        # retrieve logits
+        logits = model(input_values).logits
+
+        # take argmax and decode
+        predicted_ids = torch.argmax(logits, dim=-1)
+        transcription = processor.batch_decode(predicted_ids)
+        return {'status': True, 'text': transcription}
+    except:
+        return {'status': False, 'text': '', 'message': 'Something Went Wrong!'}
+
+# audio_file = "output.wav"
+# text = Voice_Translation(audio_file)
+# print(text)
